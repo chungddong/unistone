64f04c27286534d00e16e853b3fa49e8ce4927a3
chungddong <chungman010918@gmail.com>
2024-11-29 02:14:22 +0900

    스크랩 기능 위한 SCRAP,SCRAPCHAT 엔티티&레포지터리 생성

---
+++ src/main/java/com/sophra/unistone/Repository/ScrapChatRepository.java
@@ -0,0 +1,7 @@
+package com.sophra.unistone.Repository;
+
+import com.sophra.unistone.Entity.ScrapChat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScrapChatRepository extends JpaRepository<ScrapChat, Long> {
+}
---
+++ src/main/java/com/sophra/unistone/Repository/ScrapRepository.java
@@ -0,0 +1,7 @@
+package com.sophra.unistone.Repository;
+
+import com.sophra.unistone.Entity.Scrap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScrapRepository extends JpaRepository<Scrap, Long> {
+}
