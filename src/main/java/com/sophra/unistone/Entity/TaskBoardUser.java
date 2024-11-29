02a0e0ca36c159a93c7835b080a2f7af2a24a88b
chungddong <chungman010918@gmail.com>
2024-11-29 19:53:43 +0900

    TaskBoardDTO 에 작업보드 참가자 id 받을수 있도록 수정, TaskBoardUser 엔티티&레포지터리&서피스 생성, 작업보드 사용자 추가 및 작업보드 리스트 유저 목록 반환 기능 추가

--- src/main/java/com/sophra/unistone/Entity/TaskBoardUser.java
+++ src/main/java/com/sophra/unistone/Entity/TaskBoardUser.java
@@ -1,4 +1,31 @@
 package com.sophra.unistone.Entity;
 
-public class TaskBoardUser {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.catalina.User;
+
+@Entity
+@Table(name = "TaskBoardUser")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskBoardUser { //작업보드 사용자
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user; // Users 관계 정의
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project; // Project 관계 정의
+
+
 }
