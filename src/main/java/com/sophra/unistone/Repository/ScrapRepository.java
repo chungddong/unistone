9353f87eb75357a75d6754e1c97a723568f71aa5
chungddong <chungman010918@gmail.com>
2024-11-29 15:54:46 +0900

    scrap 목록 생성을 위한 scrapDTO 추가 및 스크랩 생성 요청, 스크랩 목록 가져오기 요청, 스크랩 내용 가져오기 요청 구현

--- src/main/java/com/sophra/unistone/Repository/ScrapRepository.java
+++ src/main/java/com/sophra/unistone/Repository/ScrapRepository.java
@@ -1,7 +1,15 @@
 package com.sophra.unistone.Repository;
 
+import com.sophra.unistone.Entity.Project;
 import com.sophra.unistone.Entity.Scrap;
+import com.sophra.unistone.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
+
+    // 유저와 프로젝트로 스크랩 찾기
+    List<Scrap> findByUserAndProject(Users user, Project project);
+    
 }
